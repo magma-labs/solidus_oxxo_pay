81ca95c2d2bd4090c639d9fa917c28d52100814b
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-17 19:51:42 -0500

    Adapt code to check the previous order on conketa and use it

--- app/models/spree/conekta_oxxo_payment.rb
+++ app/models/spree/conekta_oxxo_payment.rb
@@ -2,79 +2,51 @@ module Spree
   class ConektaOxxoPayment < Spree::PaymentSource
     attr_accessor :server, :test_mode, :api_key
 
-    def authorize(money, creditcard, gateway_options)
-
+    def authorize(money, source, gateway_options)
       ::Conekta.api_key = api_key
       ::Conekta.api_version = "2.0.0"
 
-      order = Spree::Order.find_by(number: creditcard.number)
-
       begin
-        response = ::Conekta::Order.create(payload(order))
-        creditcard.update_attribute :conekta_order_id, response.id
-        ActiveMerchant::Billing::Response.new(true, 'Orden creada satisfactoriamente', {}, parse_response(response))
+        order = Spree::Order.find(gateway_options[:originator].order_id)
+
+        unless order.conekta_order_id
+          conekta_order = ::Conekta::Order.create(payload(order))
+        end
+
+        conekta_order = ::Conekta::Order.find(order.conekta_order_id)
+        source.update_attribute(:conekta_order_id, conekta_order.id)
+        ActiveMerchant::Billing::Response.new(true, 'Orden creada satisfactoriamente', {}, parse_response(conekta_order))
       rescue ::Conekta::Error => error
         ActiveMerchant::Billing::Response.new(false, error.details.map(&:message).join(', '))
       end
     end
 
-    def name
-      'Oxxo Pay'
-    end
-
-    def month
-      payment_method.updated_at.month
-    end
-
-    def year
-      payment_method.updated_at.year
-    end
-
-    def cc_type
-      'N/A'
-    end
-
-    def last_digits
-      'N/A'
-    end
-
-    def actions
-      %w(void)
-    end
-
-    def gateway_customer_profile_id
-      user_id
-    end
-
-    def gateway_payment_profile_id
-      id
-    end
-
     private
+
     def payload(order)
       {
-          currency: 'MXN',
-          customer_info: {
-              name: order.ship_address.full_name,
-              email: order.email,
-              phone: order.ship_address.phone
-          },
-          charges: [
-              {
-                  payment_method: {
-                      type: 'oxxo_cash'
-                  }
-              }
-          ],
-          line_items: order_line_items(order),
-          shipping_lines: shipping_lines(order),
-          shipping_contact: {
-              address: {
-                  street1: order.ship_address.address1,
-                  postal_code: order.ship_address.zipcode,
-                  country: 'mx'
-              }
+        currency: 'MXN',
+        customer_info: {
+          name: order.ship_address.full_name,
+          email: order.email,
+          phone: order.ship_address.phone
+        },
+        charges: [
+          {
+            payment_method: {
+              type: 'oxxo_cash'
+            }
           }
+        ],
+        line_items: order_line_items(order),
+        shipping_lines: shipping_lines(order),
+        shipping_contact: {
+          address: {
+            street1: order.ship_address.address1,
+            postal_code: order.ship_address.zipcode,
+            country: 'MX'
+          }
+        }
       }
     end
 
@@ -84,8 +56,8 @@ module Spree
 
     def parse_response(response)
       {
-          authorization: response.charges.first.payment_method.reference,
-          conekta_order_id: response.id
+        authorization: response.charges.first.payment_method.reference,
+        conekta_order_id: response.id
       }
     end
 
