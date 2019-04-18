fb65ebe6b723b9e1605936716ab1f353d0744364
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-04-18 13:34:27 -0500

    Fix rubocop warnings

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module Spree::BaseHelper
   def conekta_oxxo_receipt(order = nil)
     @order ||= order
     if @order.paid_with?('Oxxo')
-      response_code = @order.payments&.last.response_code
+      response_code = @order.payments&.last&.response_code
       render partial: 'spree/conekta/payments/conekta_oxxo', locals: { order: @order, response_code: response_code }
     end
   end
